4a14b36853e11a2230a5d459d237bce00224bfe6
Gerson Ferreira Junior <gerson.fj@gmail.com>
2023-03-13 12:13:04 -0300

    Adding spring boot project

---
+++ src/main/java/com/gfjunior/onlinesellsapi/OnlineSellsApiApplication.java
@@ -0,0 +1,13 @@
+package com.gfjunior.onlinesellsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineSellsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineSellsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gfjunior/onlinesellsapi/OnlineSellsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gfjunior.onlinesellsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineSellsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
